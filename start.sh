023bc482e149a6ebc513ca004727405cc683aa30
Colin <computercolin@gmail.com>
2023-06-14 23:22:24 -0600

    Expose several UDP ports as well as two additional TCP ports.

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 timezone="America/Denver"
 TAGNAME="$(cat TAGNAME)"
 
-docker run --privileged -ti -d --log-opt max-file=1 --log-opt max-size=4m -e "TZ=$timezone" -v "$HOME:/home" \
-  -p 2222:22 -p 8000:80 -p 8001:8001 -p 8002:8002 -p 8003:8003 -p 8004:8004 -p 8005:8005 -p 8006:8006 -p 8007:8007 -p 8008:8008  "$TAGNAME:latest" 
+docker run --privileged -ti -d --log-opt max-file=1 --log-opt max-size=4m -e "TZ=$timezone" -v "$HOME:/h" \
+  -p 2222:22 \
+  -p 8081:80 \
+  -p 8001-8010:8001-8010 \
+  -p 9001-9010:9001-9010/udp \
+  "$TAGNAME:latest" 
 
